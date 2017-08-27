e07bc7b2cde8b95852757628516d8977ba2e5c76
joke <277637411@qq.com>
2017-08-27 16:10:23 +0800

    add scope func

--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,23 @@
-'use strict';
-function Scope(){
-    
+"use strict";
+var _ = require("lodash");
+
+function Scope() {
+  this.$$watchers = [];
 }
 
-module.exports=Scope;
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
+module.exports = Scope;
