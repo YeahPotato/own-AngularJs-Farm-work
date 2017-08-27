e177447adfda40bd777c8972851b0cdf998c794f
joke <277637411@qq.com>
2017-08-27 16:10:34 +0800

    add hello test

--- test/hello_spec.js
+++ test/hello_spec.js
@@ -1,6 +1,6 @@
 var sayHello = require('../src/hello');
 describe('Hello', function () {
     it('says hello', function () {
-        expect(sayHello('joke')).toBe('Hello, joke!');
+        expect(sayHello('joke')).toBe('hello,joke!');
     });
 });
